34136e1f0af475c0ba552c543a0a35aa238c93a2
Avneet kaur <avneetbagga06@gmail.com>
2025-05-28 00:04:17 +0530

    seventh commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ import { stripeWebhooks } from './controllers/orderController.js';
 dotenv.config();
 
 const PORT=3000;
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173','https://grocery-shopping-three.vercel.app']
 
 
 
