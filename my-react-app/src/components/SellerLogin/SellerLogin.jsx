c5201e75cde67f1dfa29a55523006661652d3c73
Avneet kaur <avneetbagga06@gmail.com>
2025-05-29 18:31:20 +0530

    backend  commit

--- my-react-app/src/components/SellerLogin/SellerLogin.jsx
+++ my-react-app/src/components/SellerLogin/SellerLogin.jsx
@@ -13,7 +13,9 @@ const SellerLogin = () => {
     const onSubmitHandler =  async (event) => {
         try{
         event.preventDefault();
-        const {data} = await axios.post('/api/seller/login', { email, password });
+        const {data} = await axios.post('/api/seller/login', { email, password },  {
+  withCredentials: true,
+});
         if(data.success){
             setIsSeller(true);
             navigate('/seller');
